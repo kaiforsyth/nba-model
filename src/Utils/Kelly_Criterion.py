c20e2550165de5c0d25a20337d8c759baf1f5eaa
Kai Forsyth <hello@kaiforsyth.com>
2023-07-15 21:26:42 +1000

    fix kelly ods int bug

--- src/Utils/Kelly_Criterion.py
+++ src/Utils/Kelly_Criterion.py
@@ -2,6 +2,7 @@ def american_to_decimal(american_odds):
     """
     Converts American odds to decimal odds (European odds).
     """
+    american_odds = int(american_odds)
     if american_odds >= 100:
         decimal_odds = (american_odds / 100)
     else:
@@ -12,6 +13,7 @@ def calculate_kelly_criterion(american_odds, model_prob):
     """
     Calculates the fraction of the bankroll to be wagered on each bet
     """
+    american_odds = int(american_odds)
     decimal_odds = american_to_decimal(american_odds)
     bankroll_fraction = round((100 * (decimal_odds * model_prob - (1 - model_prob))) / decimal_odds, 2)
     return bankroll_fraction if bankroll_fraction > 0 else 0
